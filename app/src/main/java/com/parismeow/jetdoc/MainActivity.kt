15912814e0fd7ed74ddb6f0dcf0462958dd4b561
Jayesh Mann <jayeshmann06@gmail.com>
2021-07-29 18:38:04 +0530

    Introducing Scaffold

--- app/src/main/java/com/parismeow/jetdoc/MainActivity.kt
+++ app/src/main/java/com/parismeow/jetdoc/MainActivity.kt
@@ -1,12 +1,13 @@
 package com.parismeow.jetdoc
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.tooling.preview.Preview
 import com.parismeow.jetdoc.ui.theme.JetDocTheme
 
@@ -17,7 +18,7 @@ class MainActivity : ComponentActivity() {
             JetDocTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    MainScreen()
                 }
             }
         }
@@ -25,14 +26,24 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MainScreen() {
+    Scaffold(topBar = { TopAppBar(title = { Text(text = "JetDoc") }) }) {
+
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_NO, showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun LightPreview() {
     JetDocTheme {
-        Greeting("Android")
+        MainScreen()
+    }
+}
+
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
+@Composable
+fun DarkPreview() {
+    JetDocTheme {
+        MainScreen()
     }
 }
