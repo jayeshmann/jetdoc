7fd6f6d5573144f10ae8caff837e857475e4be14
Jayesh Mann <jayeshmann06@gmail.com>
2021-07-30 15:22:54 +0530

    Open File Picker & Permissions

--- app/src/main/java/com/parismeow/jetdoc/MainActivity.kt
+++ app/src/main/java/com/parismeow/jetdoc/MainActivity.kt
@@ -1,33 +1,117 @@
 package com.parismeow.jetdoc
 
+import android.content.Intent
 import android.content.res.Configuration
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
-import androidx.activity.ComponentActivity
+import android.provider.DocumentsContract
+import android.provider.MediaStore
+import android.provider.MediaStore.Files
+import android.provider.Settings
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Add
+import androidx.compose.material.icons.outlined.Menu
+import androidx.compose.material.icons.outlined.MoreVert
+import androidx.compose.material.icons.outlined.Search
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.core.app.ActivityCompat
+import androidx.core.app.ActivityCompat.startActivityForResult
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.rememberMultiplePermissionsState
 import com.parismeow.jetdoc.ui.theme.JetDocTheme
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+    @ExperimentalPermissionsApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             JetDocTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    MainScreen()
+                val multiplePermissionsState = rememberMultiplePermissionsState(
+                    listOf(
+                        android.Manifest.permission.READ_EXTERNAL_STORAGE,
+                    )
+                )
+                RequestPermissions(
+                    multiplePermissionsState = multiplePermissionsState,
+                    navigateToSettingsScreen =
+                    {
+                        startActivity(
+                            Intent(
+                                Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                                Uri.fromParts("package", packageName, null)
+                            )
+                        )
+                    }
+                ) {
+                    Surface(color = MaterialTheme.colors.background) {
+                        MainScreen(onFABClick = { openFile(Uri.parse("/storage/emulated/0/")) })
+                    }
                 }
+
             }
         }
     }
+
+    // Request code for selecting a PDF document.
+    private fun openFile(
+        pickerInitialUri: Uri = Uri.parse("/storage/emulated/0/")
+    ) {
+        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
+            addCategory(Intent.CATEGORY_OPENABLE)
+            type = "application/pdf"
+
+            // Optionally, specify a URI for the file that should appear in the
+            // system file picker when it loads.
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                putExtra(DocumentsContract.EXTRA_INITIAL_URI, pickerInitialUri)
+            }
+        }
+        println("intent:$intent")
+        println("PICK_PDF:$PICK_PDF_FILE")
+        startActivityForResult(intent, PICK_PDF_FILE)
+    }
 }
 
+const val PICK_PDF_FILE = 2
+
+
 @Composable
-fun MainScreen() {
-    Scaffold(topBar = { TopAppBar(title = { Text(text = "JetDoc") }) }) {
+fun MainScreen(modifier: Modifier = Modifier, onFABClick: () -> Unit) {
+    val scaffoldState = rememberScaffoldState()
+    Scaffold(
+        scaffoldState = scaffoldState,
+        topBar = { TopAppBar(title = { Text(text = "JetDoc") }) },
+        floatingActionButton = {
+            FloatingActionButton(onClick = { onFABClick() }) {
+                Icon(imageVector = Icons.Outlined.Add, contentDescription = "Add")
+            }
+        },
+        floatingActionButtonPosition = FabPosition.Center,
+        isFloatingActionButtonDocked = true,
+        bottomBar = {
+            BottomAppBar(cutoutShape = CircleShape) {
+                IconButton(onClick = { /* doSomething() */ }) {
+                    Icon(Icons.Outlined.Menu, contentDescription = "Dummy Menu")
+                }
+                // The actions should be at the end of the BottomAppBar
+                Spacer(Modifier.weight(weight = 1f, fill = true))
+                IconButton(onClick = { /* doSomething() */ }) {
+                    Icon(Icons.Outlined.Search, contentDescription = "Dummy Search")
+                }
+                IconButton(onClick = { /* doSomething() */ }) {
+                    Icon(Icons.Outlined.MoreVert, contentDescription = "Dummy Options")
+                }
+            }
+        }) {
 
     }
 }
@@ -36,7 +120,7 @@ fun MainScreen() {
 @Composable
 fun LightPreview() {
     JetDocTheme {
-        MainScreen()
+        MainScreen() {}
     }
 }
 
@@ -44,6 +128,6 @@ fun LightPreview() {
 @Composable
 fun DarkPreview() {
     JetDocTheme {
-        MainScreen()
+        MainScreen() {}
     }
 }
---
+++ app/src/main/java/com/parismeow/jetdoc/RequestPermissions.kt
@@ -0,0 +1,117 @@
+package com.parismeow.jetdoc
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.MultiplePermissionsState
+import com.google.accompanist.permissions.PermissionState
+
+
+@OptIn(ExperimentalPermissionsApi::class)
+@Composable
+fun RequestPermissions(
+    multiplePermissionsState: MultiplePermissionsState,
+    navigateToSettingsScreen: () -> Unit,
+    onGrantedContents: @Composable () -> Unit
+) {
+    // Track if the user doesn't want to see the rationale any more.
+    var doNotShowRationale by rememberSaveable { mutableStateOf(false) }
+
+    when {
+        // If all permissions are granted, then show screen with the feature enabled
+        multiplePermissionsState.allPermissionsGranted -> {
+//            Text("Camera and Read storage permissions Granted! Thank you!")
+            // A surface container using the 'background' color from the theme
+            onGrantedContents()
+        }
+        // If the user denied any permission but a rationale should be shown, or the user sees
+        // the permissions for the first time, explain why the feature is needed by the app and
+        // allow the user decide if they don't want to see the rationale any more.
+        multiplePermissionsState.shouldShowRationale ||
+                !multiplePermissionsState.permissionRequested -> {
+            if (doNotShowRationale) {
+                Text("Feature not available")
+            } else {
+                Column {
+                    val revokedPermissionsText = getPermissionsText(
+                        multiplePermissionsState.revokedPermissions
+                    )
+                    Text(
+                        "$revokedPermissionsText important. " +
+                                "Please grant all of them for the app to function properly."
+                    )
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Row {
+                        Button(
+                            onClick = {
+                                multiplePermissionsState.launchMultiplePermissionRequest()
+                            }
+                        ) {
+                            Text("Request permissions")
+                        }
+                        Spacer(Modifier.width(8.dp))
+                        Button(onClick = { doNotShowRationale = true }) {
+                            Text("Don't show rationale again")
+                        }
+                    }
+                }
+            }
+        }
+        // If the criteria above hasn't been met, the user denied some permission. Let's present
+        // the user with a FAQ in case they want to know more and send them to the Settings screen
+        // to enable them the future there if they want to.
+        else -> {
+            Column {
+                val revokedPermissionsText = getPermissionsText(
+                    multiplePermissionsState.revokedPermissions
+                )
+                Text(
+                    "$revokedPermissionsText denied. See this FAQ with " +
+                            "information about why we need this permission. Please, grant us " +
+                            "access on the Settings screen."
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                Button(onClick = navigateToSettingsScreen) {
+                    Text("Open Settings")
+                }
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalPermissionsApi::class)
+private fun getPermissionsText(permissions: List<PermissionState>): String {
+    val revokedPermissionsSize = permissions.size
+    if (revokedPermissionsSize == 0) return ""
+
+    val textToShow = StringBuilder().apply {
+        append("The ")
+    }
+
+    for (i in permissions.indices) {
+        textToShow.append(permissions[i].permission)
+        when {
+            revokedPermissionsSize > 1 && i == revokedPermissionsSize - 2 -> {
+                textToShow.append(", and ")
+            }
+            i == revokedPermissionsSize - 1 -> {
+                textToShow.append(" ")
+            }
+            else -> {
+                textToShow.append(", ")
+            }
+        }
+    }
+    textToShow.append(if (revokedPermissionsSize == 1) "permission is" else "permissions are")
+    return textToShow.toString()
+}
